c40e17d401bc3addb047531dfd0559b3e79ea805
Benoit TELLIER <btellier@linagora.com>
2026-04-17 22:10:57 +0200

    Allowed URL resolver

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  test: {
+    environment: 'node',
+  },
   plugins: [react()],
   server: {
     host: true,
